93f5e6e031805e73fedd0ce6de4e4df273913e1b
Hairay <hairay_lin@avision.com.tw>
2021-10-18 11:41:05 +0800

    sh: declare: not found

--- s3test.sh
+++ s3test.sh
@@ -4,8 +4,9 @@ while true
 do
     echo mem > /sys/power/state
 	busybox devmem 0x88CF0004 32 >> s3test.log
-	declare -i num=$RANDOM*30/32768+15
-    sleep $num
+	num=$RANDOM*30/32768+15
+	echo $((num))
+    sleep $((num))
 done
 
 
